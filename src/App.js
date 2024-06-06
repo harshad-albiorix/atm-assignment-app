9b007273026eca9f071f770f8811731a9e752308
harshad-albiorix <harshadr.albiorix@gmail.com>
2024-06-06 13:29:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { ATMPage } from "./pages";
+
+function App() {
+  return (
+    <div className="w-full h-full main-container">
+      <ATMPage />
+    </div>
+  );
+}
+
+export default App;
